2294865969ded957a8fb9c930c021d65df5903cb
Leandro Grando <leandro.franciosigrando@gmail.com>
2022-11-16 21:42:35 -0300

    feat: add error and stopping logic.

--- src/WorkerService/Worker.cs
+++ src/WorkerService/Worker.cs
@@ -3,18 +3,37 @@ namespace WorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IHostApplicationLifetime hostApplicationLifetime)
         {
             _logger = logger;
+            _hostApplicationLifetime = hostApplicationLifetime;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation("Worker running in container at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            try
+            {
+                _logger.LogInformation("Worker running in container at: {time}", DateTimeOffset.Now);
 
-            _logger.LogInformation("Finished awaiting");
+                //throw new Exception("forced error");
+
+                // Execute some business processing.
+                await Task.Delay(5000, stoppingToken);
+
+                _logger.LogInformation("Finished processing at: {time}.", DateTimeOffset.Now);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred at cron job app execution.");
+                throw;
+            }
+            finally
+            {
+                _logger.LogInformation("Forcing stop at: {time}.", DateTimeOffset.Now);
+                _hostApplicationLifetime.StopApplication();
+            }
         }
     }
 }
